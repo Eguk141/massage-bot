73ecf7cf4061691a6d33ed2c3d851d5574b30db2
Eguk141 <tetiana.arich3@gmail.com>
2026-04-11 19:38:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,18 +8,6 @@ from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, ReplyKey
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram import F
 from aiogram.types import CallbackQuery
-@dp.callback_query(F.data.startswith("cancel_"))
-async def cancel_booking(callback: CallbackQuery):
-    date = callback.data.split("_")[1]
-
-    if date in db["bookings"]:
-        db["bookings"].pop(date)
-
-    save()
-
-    await callback.message.answer("❌ Запис скасовано")
-    await callback.answer()
-    @dp.callback_query(F.data.startswith("move_"))
 async def move_booking(callback: CallbackQuery):
     await callback.message.answer("🔄 Функція переносу скоро буде 😄")
     await callback.answer()
